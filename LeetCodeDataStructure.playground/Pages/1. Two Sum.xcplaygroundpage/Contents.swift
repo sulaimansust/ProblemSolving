364ebca7d3d6aa3401e90cdfccffb1ea6f65f293
Sulayman Md <111493533+sulayman-bav@users.noreply.github.com>
2022-10-01 18:25:05 +0200

    Two sum problem is solved now

---
+++ LeetCodeDataStructure.playground/Pages/1. Two Sum.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    
+    for i in 0..<nums.count {
+        for j in 1..<nums.count {
+            if nums[i] + nums[j] == target && i != j {
+                return [i,j]
+            }
+        }
+    }
+    
+    
+    return []
+}
+
+var numbers = [2,7,11,15]
+
+twoSum(numbers, 9)
+
+
+//: [Next](@next)
