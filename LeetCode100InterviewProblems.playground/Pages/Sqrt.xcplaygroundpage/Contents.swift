f0141dd973fa706b05fef512a685db5b37f4da78
Sulayman Md <sulaiman.sust@gmail.com>
2022-12-30 22:49:01 +0100

    Square root

---
+++ LeetCode100InterviewProblems.playground/Pages/Sqrt.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func mySqrt(_ x: Int) -> Int {
+//    return Int(Float(x).squareRoot())
+    return Int(Double(x).squareRoot())
+    
+}
+
+
+mySqrt(2147395599)
+
+//: [Next](@next)
